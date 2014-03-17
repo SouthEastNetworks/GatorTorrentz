d1c9409338f569d6ad1990f58f2f0cd3aa9d207c
Pavan <Pavan@Pavan-PC>
2014-03-17 02:46:48 -0400

    Added new methods

---
+++ GatorTorrentz/src/BitField.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+
+public class BitField 
+{
+	int messageLength;
+	int messageType;
+	int[] bitFieldArray;
+	public static byte[] arrayToByte(int[] bitArray) {
+		// TODO add a method in Filecheck to return the array
+		bitArray = Filecheck.getChunkArray();
+		int numParts = bitArray.length;
+		int i = 0;
+		String bitStr = "";
+		while (i < numParts) {
+			bitStr += bitArray[i];
+			i++;
+		}
+		byte[] byteChunk = bitStr.getBytes();
+		System.out.println(new String(byteChunk));
+		return byteChunk;
+	}
+	public static void sendByteBitField() {
+		// TODO method to send the bitfield message
+	}
+	public static void main(String[] args) {
+		int[] bitArray = new int[10];
+		byte[] dummy2 = arrayToByte(bitArray);
+	}
+}
--- GatorTorrentz/src/Filecheck.java
+++ GatorTorrentz/src/Filecheck.java
@@ -3,6 +3,22 @@
 import java.io.*;
 
 public class Filecheck {
+	public static int[] getChunkArray() { 
+		String fname = "src/file/somefile.mp3";
+		int[] egarray = new int[10];// need to get number of chunks the file has
+		// been split into and give that size
+		String filename;
+		for (int i = 0; i < 10; i++) {
+			filename = fname + ".part" + i;
+			File f = new File(filename);
+			if (f.exists() && !f.isDirectory()) {
+				egarray[i] = 1;
+			} else {
+				egarray[i] = 0;
+			}
+		}
+		return egarray;
+	}
 	public static void main(String[] args) {
 		String fname = "src/file/somefile.mp3";
 		// int num=new File(ifile.getParentFile().getAbsolutePath()).listFiles().length;
